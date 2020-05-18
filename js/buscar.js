bab608d5f6f207e3a7242f4bf0bc3f3f70e58599
Vinicius Almeida <viniciusnalm@gmail.com>
2020-05-18 15:02:57 -0300

    adding stakeholder list

--- js/buscar.js
+++ js/buscar.js
@@ -1,6 +1,29 @@
-var inputBusca = document.querySelector("#searchInput");
+let botao = document.querySelector(".btn-search");
 
 
-inputBusca.addEventListener("input", function() {
-    console.log(this.value);
+botao.addEventListener("click", function() {
+   
+    setInputValue(catchInput());
+    showWrapper();
+
 });
+
+
+
+// functions used below
+
+function showWrapper() {
+    document.querySelector(".wrapper").classList.add("wrapper-show");
+}
+
+
+function catchInput() {
+    let inputSearch = document.querySelector("#searchInput");
+    return inputSearch;
+}
+
+
+function setInputValue (searchBarInput) {
+    document.querySelector(".cityName").textContent = searchBarInput.value;
+}
+
